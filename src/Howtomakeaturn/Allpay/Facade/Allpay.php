fcbde679e9d6d1fd24e5d3dbaeb53a4bb668a6ed
fangwenjuan <fangwenjuan@kollway.com>
2016-02-25 18:23:05 +0800

    init

--- src/Howtomakeaturn/Allpay/Facade/Allpay.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Kollway\Allpay\Facade;
-use Illuminate\Support\Facades\Facade;
-
-class Allpay extends Facade {
-
-    protected static function getFacadeAccessor() { return 'allpay'; }
-
-}
